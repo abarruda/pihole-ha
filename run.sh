92b7f35c636b225dcf73c9c50c2c15b330f663b7
Aaron Arruda <aaron@abarruda.com>
2018-12-07 16:42:16 -0800

    Mounting specific files, specifying timezone and password

--- run.sh
+++ run.sh
@@ -17,12 +17,13 @@ docker run -d \
     -p 67:67/udp \
     -p 80:80 \
     -p 443:443 \
-    -v "$(pwd)/config/pihole/:/etc/pihole/" \
-    -v "$(pwd)/config/dnsmasq.d/:/etc/dnsmasq.d/" \
-    -v "$(pwd)/config/hosts:/etc/hosts" \
-    -v "${TEMP_USAGE_DB_PATH}${TEMP_USAGE_DB_FILE}:/etc/pihole/${TEMP_USAGE_DB_FILE}" \
+    -v "$(pwd)/config/pihole/adlists.list:/etc/pihole/adlists.list" \
+    -v "$(pwd)/config/pihole/whitelist.txt:/etc/pihole/whitelist.txt" \
+    -v "$(pwd)/config/dnsmasq.d/custom-dns.conf:/etc/dnsmasq.d/custom-dns.conf" \
     -e ServerIP="$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="src") print $(i+1)}')" \
     -e DNS1=8.8.8.8 \
     -e DNS2=208.67.222.222 \
+    -e TZ=America/Los_Angeles \
+    -e WEBPASSWORD=$PIHOLE_PASS \
     --restart=unless-stopped \
     pihole/pihole:4.0.0-1_armhf
