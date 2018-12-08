2edd29b082ce8d2aefc72200f084e549b5468a90
Aaron Arruda <aaron@abarruda.com>
2018-12-07 17:06:13 -0800

    Persist pihole configurations

--- run.sh
+++ run.sh
@@ -2,21 +2,13 @@
 
 set -x
 
-touch config/hosts
-
-TEMP_USAGE_DB_PATH=/tmp/pihole-usage-db-vol/
-TEMP_USAGE_DB_FILE=pihole-FTL.db
-
-mkdir -p $TEMP_USAGE_DB_PATH
-touch $TEMP_USAGE_DB_PATH$TEMP_USAGE_DB_FILE
-
-
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
     -p 67:67/udp \
     -p 80:80 \
     -p 443:443 \
+    -v "$(pwd)/config/pihole/setupVars.conf:/etc/pihole/setupVars.conf" \
     -v "$(pwd)/config/pihole/adlists.list:/etc/pihole/adlists.list" \
     -v "$(pwd)/config/pihole/whitelist.txt:/etc/pihole/whitelist.txt" \
     -v "$(pwd)/config/dnsmasq.d/custom-dns.conf:/etc/dnsmasq.d/custom-dns.conf" \
